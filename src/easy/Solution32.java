f515afd8a944b293cdc80d115412a0bd3a2b0410
reeedking <121719158@qq.com>
2021-03-26 08:18:41 +0800

    【leetcode】环形链表

---
+++ src/easy/Solution32.java
@@ -0,0 +1,18 @@
+package easy;
+
+import model.ListNode;
+
+// 环形链表
+public class Solution32 {
+
+    public boolean hasCycle(ListNode head) {
+        ListNode fast, slow;
+        fast = slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) return true;
+        }
+        return false;
+    }
+}
