5724d92ceffc0bda9a056d6b27956580910b0c0a
NaN <121719158@qq.com>
2021-03-28 15:15:46 +0800

    【LeetCode】 最小覆盖子串

---
+++ src/hard/Solution03.java
@@ -0,0 +1,42 @@
+package hard;
+
+import java.util.HashMap;
+import java.util.Map;
+
+// 最小覆盖子串
+public class Solution03 {
+    public String minWindow(String s, String t) {
+        int n = s.length(), m = t.length();
+        if(n < m) return "";
+        Map<Character, Integer> need = new HashMap<>();
+        for(int i = 0; i < m; i++){
+            char c = t.charAt(i);
+            need.put(c, need.getOrDefault(c, 0) + 1);
+        }
+
+        int[] res = new int[]{0, n};
+        int left = 0, needCnt = m;
+        for(int right = 0; right < n; right++){
+            char cR = s.charAt(right);
+            if(need.getOrDefault(cR, 0) > 0){
+                needCnt--;
+            }
+            need.put(cR, need.getOrDefault(cR, 0) - 1);
+            if(needCnt == 0){
+                while(true){
+                    char cL = s.charAt(left);
+                    if(need.get(cL) == 0){
+                        break;
+                    }
+                    need.put(cL, need.get(cL) + 1);
+                    left++;
+                }
+                if(right - left < res[1] - res[0]){
+                    res[0] = left;
+                    res[1] = right;
+                }
+            }
+        }
+        return res[0] != 0 || res[1] != n ? s.substring(res[0], res[1] + 1) : "";
+    }
+}
