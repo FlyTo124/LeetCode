b4cbebe5dc03c5b634a3cf0e9829b0046440f836
reeedking <reeedking@gmail.com>
2021-04-20 16:53:36 +0800

    【leetcode】 俄罗斯套娃信封问题

---
+++ src/hard/Solution04.java
@@ -0,0 +1,43 @@
+package hard;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+//俄罗斯套娃信封问题
+public class Solution04 {
+
+    public int maxEnvelopes(int[][] envelopes) {
+        int n = envelopes.length;
+
+        Arrays.sort(envelopes, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] a, int[] b) {
+                return a[0] == b[0] ? b[1] - a[1] : a[0] - b[0];
+            }
+        });
+        int[] height = new int[n];
+        for (int i = 0; i < n; i++) {
+            height[i] = envelopes[i][1];
+        }
+        return lengthOfLIS(height);
+    }
+
+    private int lengthOfLIS(int[] nums) {
+        int[] dp = new int[nums.length];
+
+        Arrays.fill(dp, 1);
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+
+        int res = 0;
+        for (int i = 0; i < dp.length; i++) {
+            res = Math.max(res, dp[i]);
+        }
+        return res;
+    }
+}
